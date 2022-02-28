2e72aeace8b1b2aefbe1d0fb5804b3d54190c939
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2022-02-28 16:33:29 +0700

    initialize app

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
